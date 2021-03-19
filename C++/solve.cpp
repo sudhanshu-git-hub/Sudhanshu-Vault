c212198542dc57072a63909938a920e7408946e3
Sudhanshu <57716795+sudhanshu-git-hub@users.noreply.github.com>
2021-03-19 22:47:11 +0530

    add files first time in c++

---
+++ C++/solve.cpp
@@ -0,0 +1,30 @@
+
+#include <bits/stdc++.h>
+//using namespace std;
+
+void c_func(){
+    int a;
+    scanf("%d",&a);
+    printf("\nNumber=%d",a);
+}
+
+std::string get_line(){
+    std::string x;
+    std::getline(std::cin,x);
+    return x;
+}
+
+int main() {
+    // int a,b;
+    // std::string x;
+    // std::cin>>a>>b>>x;
+    // std::cout<<a<<" "<<b<<" "<<x<<"\n";
+    // printf("Sudhanshu");
+    // c_func();
+    //std::cout<<get_line();
+    freopen("data.txt","r",stdin);
+    freopen("op.txt","w",stdout);
+}
+
+
+

bd06aa0f4c52514efa6fd05ee9e971a59df5e58b
Sudhanshu Singh <57716795+sudn2s@users.noreply.github.com>
2022-05-09 23:21:43 +0530

    new youtube summarize

--- Apps/Youtube-Transcription/app.py
+++ Apps/Youtube-Transcription/app.py
@@ -5,7 +5,8 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
 from transformers import pipeline
 from flask import request
-
+from transformers import BartForConditionalGeneration, BartTokenizer
+from typing import List
 
 # define a variable to hold you app
 app = Flask(__name__)
@@ -25,6 +26,15 @@ def summarizeTranscript(youtubeID):
     summary_text = summarization(text_transcript)[0]['summary_text']
     return summary_text
 
+def summarizeTranscriptNew(youtubeID):
+    tokenizer = BartTokenizer.from_pretrained('bart-large-cnn')
+    text_transcript = getTextTranscript(youtubeID)
+    model = BartForConditionalGeneration.from_pretrained('bart-large-cnn')
+    input_ids = tokenizer.batch_encode_plus(text_transcript, return_tensors='pt', max_length=2000)['input_ids']
+    summary_ids = model.generate(input_ids)
+    summaries = [tokenizer.decode(s) for s in summary_ids]
+    return summaries
+
 def getVideoId(youtube_url):
      id = youtube_url.split("v=")
      videoID = id[1][:11]
@@ -33,33 +43,23 @@ def getVideoId(youtube_url):
 # define your resource endpoints
 @app.route('/')
 def index_page():
-    #print(getTextTranscript('YgX97OjSozw'))
-    #print(summarizeTranscript('YgX97OjSozw'))
     return render_template('index.html')
 
 @app.route('/home')
 def home_page():
-    #print(getTextTranscript('YgX97OjSozw'))
-    #print(summarizeTranscript('YgX97OjSozw'))
     return render_template('home.html')
+
+
 @app.route('/summarize')
 def summary_page():
-    #print(getTextTranscript('YgX97OjSozw'))
-    #print(summarizeTranscript('YgX97OjSozw'))
     args = request.args
     youtube_url =args.get("youtube_url")
     videoid = getVideoId(youtube_url)
+    thumbnail_url ='https://img.youtube.com/vi/'+videoid+'/0.jpg'
     transcipt = getTextTranscript(videoid)
     summary= summarizeTranscript(videoid)
-    return render_template('summary.html',transcript=transcipt,summary=summary)
-
-#@app.route('http://[hostname]/api/summarize?youtube_url=<url>',methods=['GET'])
-#def index_page():
-    #yotube_url = request.args['youtube_url']
-    #id = yotube_url.split("v=")
-    #videoid = id[1][:11]
-    #return summarizeTranscript(videoid)
-
+    lorem ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam massa, euismod vitae rutrum elementum, dictum nec odio. Proin lacus augue, rutrum ut dapibus eget, tincidunt sit amet leo. Fusce bibendum, urna eget blandit faucibus, erat risus commodo neque, quis auctor magna mauris quis nunc. Vestibulum eget nibh quis metus euismod eleifend elementum in odio. Suspendisse tempor felis sed magna sagittis consequat. Nunc non vulputate sapien. Proin fringilla libero quis fermentum maximus. Aenean nec vulputate dolor. Nam malesuada ipsum eros, in bibendum metus finibus id. Proin in dolor ligula.Quisque lectus nisi, eleifend vel ipsum sit amet, feugiat feugiat nibh. Proin congue eros nisi, ac euismod sapien consectetur sed. Morbi pretium felis quis dolor viverra porta. Morbi dictum nulla ac tortor porttitor, quis consectetur velit eleifend. Praesent vitae mollis felis. Aenean eu enim nec est egestas posuere et nec tortor. Morbi sed gravida tortor. Praesent commodo tempus augue a faucibus. Mauris at dolor a eros lobortis euismod eu a nisl. Mauris euismod mollis libero maximus sodales. Vestibulum ultricies augue turpis. Donec rutrum ipsum sit amet commodo pellentesque. Vivamus scelerisque, ipsum et volutpat eleifend, nunc nisi lobortis metus, sit amet ultrices erat felis nec ante. Etiam condimentum varius semper."
+    return render_template('summary.html',transcript=transcipt,summary=summary,thumbnail_url=thumbnail_url)
 
 
 
--- Apps/Youtube-Transcription/static/js/script.js
+++ Apps/Youtube-Transcription/static/js/script.js
@@ -1,15 +1,28 @@
+//hide loading animation
+function hideLoading(){
+    document.querySelector('.loading').style.visibility="hidden";
+}
+function visibleLoading(){
+    document.querySelector('.loading').style.visibility="visible";
+}
+hideLoading();
 function isValidURL(string) {
   var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
   return (res !== null)
 };
 function summarize() {
+    visibleLoading();
     let youtube_url = document.querySelector('#youtube-url').value;
     if(isValidURL(youtube_url)){
          console.log(youtube_url);
+         document.querySelector('.row-1').style.visibility='hidden'
+         document.querySelector('.row-2').style.visibility='hidden'
          window.location.replace("/summarize?youtube_url="+youtube_url);
     }
     else{
-        window.alert("Invalid URL");
+        window.alert("Invalid Youtube URL");
+        document.querySelector('#youtube-url').value=''
+        hideLoading();
     }
 
 }
